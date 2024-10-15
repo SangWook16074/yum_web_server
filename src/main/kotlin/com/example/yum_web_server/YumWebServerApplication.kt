8ec415f22a8f255383fec4846392549f8bf8708e
sangwook16074 <qazws78941@gmail.com>
2024-10-15 20:42:28 +0900

    불필요한 print 문 제거

--- src/main/kotlin/com/example/yum_web_server/YumWebServerApplication.kt
+++ src/main/kotlin/com/example/yum_web_server/YumWebServerApplication.kt
@@ -8,18 +8,5 @@ import org.springframework.boot.runApplication
 class YumWebServerApplication
 
 fun main(args: Array<String>) {
-	// .env 파일 로드
-	val dotenv = dotenv()
-
-	// 환경 변수 가져오기
-	val dbUrl = dotenv["DB_URL"]
-	val dbUsername = dotenv["DB_USERNAME"]
-	val dbPassword = dotenv["DB_PASSWORD"]
-
-	// 환경 변수를 확인하기 위한 코드
-	println("DB URL: $dbUrl")
-	println("DB Username: $dbUsername")
-	println("DB Password: $dbPassword")
-
 	runApplication<YumWebServerApplication>(*args)
 }
