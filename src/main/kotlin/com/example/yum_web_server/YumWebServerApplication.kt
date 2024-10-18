0ed7a3e11e963de87b06d2f8d89581d9fad73804
Jun9Wo <wjddnalt24@gmail.com>
2024-10-18 17:35:20 +0900

    테스트를 위한  라이브러리 추가

--- src/main/kotlin/com/example/yum_web_server/YumWebServerApplication.kt
+++ src/main/kotlin/com/example/yum_web_server/YumWebServerApplication.kt
@@ -1,6 +1,5 @@
 package com.example.yum_web_server
 
-import io.github.cdimascio.dotenv.dotenv
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
