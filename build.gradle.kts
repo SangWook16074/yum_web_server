97949b5aea9d2667633c11260f2329f99466e748
sangwook16074 <qazws78941@gmail.com>
2024-11-03 10:53:32 +0900

    1. Dockerfile 생성 Dockerfile을 통해 도커 이미지를 생성합니다.
    2. docker-compose.yml 생성
    docker-compose.yml을 통해 mysql과 서버를 동시에 빌드합니다.
    
    3. .env를 사용하기에 env파일 사용은 중지되었습니다.
    
    4. test, dev-deploy를 위한 CICD 워크플로우가 생성되었습니다.

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,8 +33,6 @@ dependencies {
 	testImplementation("io.mockk:mockk:1.13.13")
 	// Spring Boot 애플리케이션의 단위 테스트 및 통합 테스트를 위한 기본 테스트 라이브러리
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	// env 파일을 사용하기 위한 라이브러리
-	implementation("io.github.cdimascio:dotenv-kotlin:6.3.1")
 	// OpenAPI 3 문서화 및 Swagger UI 제공을 위한 라이브러리
 	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
 }
