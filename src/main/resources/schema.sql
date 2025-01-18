df35cf601391d53b75f33154ebad228b9571616f
sangwook16074 <qazws78941@gmail.com>
2025-01-18 16:48:25 +0900

    테이블 스키마 수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,17 +1,15 @@
---DROP TABLE IF EXISTS ingredient;
---DROP TABLE IF EXISTS favorite;
---
---CREATE TABLE IF NOT EXISTS ingredient (
---    id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
---    name VARCHAR(255) NOT NULL,
---    is_freezed BOOLEAN NOT NULL,
---    category VARCHAR(100) NOT NULL,
---    start_at TIMESTAMP(6) NOT NULL,
---    end_at TIMESTAMP(6) NOT NULL
---);
---
---CREATE TABLE IF NOT EXISTS favorite (
---    category VARCHAR(100) PRIMARY KEY
---);
+DROP TABLE IF EXISTS ingredient;
+DROP TABLE IF EXISTS favorite;
 
-ALTER TABLE IF EXISTS ingredient MODIFY end_at TIMESTAMP(6) NULL;
+CREATE TABLE IF NOT EXISTS ingredient (
+    id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    is_freezed BOOLEAN NOT NULL,
+    category VARCHAR(100) NOT NULL,
+    start_at TIMESTAMP(6) NOT NULL,
+    end_at TIMESTAMP(6) NULL
+);
+
+CREATE TABLE IF NOT EXISTS favorite (
+    category VARCHAR(100) PRIMARY KEY
+);
